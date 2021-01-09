37ca4ca642fb6c21a2db623de6fceb4bfc0f09e0
jessexknight <jesse.x.knight@gmail.com>
2021-01-09 15:29:17 -0500

    update admin code

--- code/admin/download.py
+++ code/admin/download.py
@@ -1,5 +1,4 @@
-import os
-import re
+import os,sys,re
 import pandas as pd
 
 def redirect(filename):
@@ -17,17 +16,24 @@ def download(filename,url):
     download(filename,url)
 
 temp = 'tmp.xlsx'
-main = os.path.join(os.environ['ROOT'],'data','main.xlsx')
-# gid = '1DVnosgUx5jc-Pev73Nouh_FfbyldRsKUdc1pcknYUmw' # v1
-gid = '1GI2g1Kg7xOFRrEpXLwJUHySkcgLrdBMt-56qwLG3ylA' # reboot
+cols = os.path.join(os.environ['ROOT'],'data','{}.cols')
+xlsx = os.path.join(os.environ['ROOT'],'data','{}.xlsx')
+gurl = 'https://docs.google.com/spreadsheets/d/{}/export'
+gids = {
+  # 'v0':   '1DVnosgUx5jc-Pev73Nouh_FfbyldRsKUdc1pcknYUmw',
+  'main': '1GI2g1Kg7xOFRrEpXLwJUHySkcgLrdBMt-56qwLG3ylA',
+  'api':  '1WwS_j7yv7W9RJ0fDUrKeejs5v6NT9Rqdk0vr5tfTMEs',
+}
+# for name,gid in gids.items():
+for name in sys.argv[1:]:
+  gid = gids[name]
+  print('downloading '+name+' ...',flush=True)
+  download(temp,gurl.format(gid))
 
-print('downloading...')
-download(temp,'https://docs.google.com/spreadsheets/d/'+gid+'/export')
-
-print('cleaning...')
-X = pd.read_excel(temp)
-# X = X[X['stage'] == 1]
-X.to_excel(main,index=False)
-os.system('rm {}'.format(temp))
+  print('cleaning '+name+' ...',flush=True)
+  X = pd.read_excel(temp)
+  X[0:0].to_csv(cols.format(name),index=False,sep='\n')
+  X.to_excel(xlsx.format(name),index=False)
+  os.system('rm {}'.format(temp))
 
 print('done')
--- code/admin/valex.py
+++ code/admin/valex.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from collections import OrderedDict as odict
 
+# TODO: validate API data
+
 def check(*oks):
   return '['+''.join('.' if ok else '#' for ok in oks)+']'
 
@@ -11,14 +13,15 @@ def rootpath(*args):
 def findfile(base,exts):
   return any(os.path.exists(base+'.'+ext) for ext in exts)
 
-def valfun(ref,X):
+def valfun(ref,X,V):
   val = odict(
     uid  = sum(ref['id']  == X['id'])  == 1,
     ubib = sum(ref['bib'] == X['bib']) == 1,
+    cov  = (ref['id'] < 0) or sum((ref['id'] == V['id']) & (ref['bib'] == V['bib'])) == 1,
     pdf  = findfile(rootpath('refs','pdf',ref['bib']),['pdf']),
     app  = findfile(rootpath('refs','pdf',ref['bib'])+'x',['pdf','zip','doc','docx']),
   )
-  val['OK'] = val['uid'] and val['ubib'] and val['pdf']
+  val['OK'] = val['uid'] and val['ubib'] and val['cov'] and val['pdf']
   return(val)
 
 def printval(ref,val):
@@ -26,14 +29,16 @@ def printval(ref,val):
     ref['bib'].rjust(20)+
     ' OK: '+check(val['OK'])+
     ' UID: '+check(val['uid'],val['ubib'])+
+    ' COV: '+check(val['cov'])+
     ' PDF: '+check(val['pdf'])+
     ' APP: '+check(val['app'])+
   '')
 
+V = pd.read_csv(rootpath('refs','covidence-included.csv'))
 X = pd.read_excel(rootpath('data','main.xlsx'))
 fail = []
 for ref in X.to_dict(orient='rows'):
-  val = valfun(ref,X)
+  val = valfun(ref,X,V)
   printval(ref,val)
   if not val['OK']: fail.append(ref['bib'])
 
