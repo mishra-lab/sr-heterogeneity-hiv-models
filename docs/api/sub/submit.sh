aa8c18983805fd4bed74ed77ac911eee998e6545
jessexknight <jesse.x.knight@gmail.com>
2021-03-31 18:52:37 -0400

    fix submit script

--- docs/api/sub/submit.sh
+++ docs/api/sub/submit.sh
@@ -4,5 +4,5 @@ pdftk $from cat  1     output $submit/title.pdf
 pdftk $from cat  2     output $submit/abstract.pdf
 pdftk $from cat  3-24  output $submit/body.pdf
 # pdftk $from cat  1-24  output $submit/main.pdf # medRxiv
-# pdftk $from cat 25-end output $submit/appendix.pdf # medRxiv
+pdftk $from cat 25-end output $submit/appendix.pdf
 cp cl/cover.pdf $submit/cover.pdf
