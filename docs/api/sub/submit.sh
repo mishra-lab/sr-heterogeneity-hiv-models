911048440ad87d24c611d66b3197369dcf6a6c38
jessexknight <jesse.x.knight@gmail.com>
2021-03-31 18:43:03 -0400

    update submit script

--- docs/api/sub/submit.sh
+++ docs/api/sub/submit.sh
@@ -1,7 +1,8 @@
 from=../main.pdf
-submit=./aids/1
+submit=./aids/2
 pdftk $from cat  1     output $submit/title.pdf
 pdftk $from cat  2     output $submit/abstract.pdf
 pdftk $from cat  3-24  output $submit/body.pdf
-pdftk $from cat 25-end output $submit/appendix.pdf
+# pdftk $from cat  1-24  output $submit/main.pdf # medRxiv
+# pdftk $from cat 25-end output $submit/appendix.pdf # medRxiv
 cp cl/cover.pdf $submit/cover.pdf
