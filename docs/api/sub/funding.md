596282c9d1ad4f4a383d3c3220f76cf448f86950
jessexknight <jesse.x.knight@gmail.com>
2021-10-17 17:37:29 -0400

    update docs/api for epidemics submit (2021-05-25)

---
+++ docs/api/sub/funding.md
@@ -0,0 +1,11 @@
+Natural Sciences and Engineering Research Council of Canada
+Award Number: CGS-D
+Recipient: Jesse Knight
+
+Ontario Early Researcher Award
+Award Number: ER17-13-043
+Recipient: Sharmistha Mishra
+
+Canadian Institutes of Health Research (CA)
+Award Number: FN-13455
+Recipient: Sharmistha Mishra
--- docs/api/sub/submit.sh
+++ docs/api/sub/submit.sh
@@ -1,8 +1,11 @@
 from=../main.pdf
-submit=./aids/2
-pdftk $from cat  1     output $submit/title.pdf
-pdftk $from cat  2     output $submit/abstract.pdf
-pdftk $from cat  3-24  output $submit/body.pdf
+submit=./tmp
+# pdftk $from cat  2     output $submit/abstract.pdf # J/AIDS
+# pdftk $from cat  1-24  output $submit/manuscript.pdf # JAIDS
+# pdftk $from cat  1     output $submit/title.pdf # AIDS
+# pdftk $from cat  3-24  output $submit/body.pdf # AIDS
 # pdftk $from cat  1-24  output $submit/main.pdf # medRxiv
-pdftk $from cat 25-end output $submit/appendix.pdf
+# pdftk $from cat 25-end output $submit/appendix.pdf # J/AIDS
+cp $from $submit/manuscript.pdf # Epidemics
+cp ../coi.txt $submit/coi.txt # Epidemics
 cp cl/cover.pdf $submit/cover.pdf
